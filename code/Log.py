2e126f80d35045641c078a5d17d3992117d686ee
Rainicy <rainicy925@gmail.com>
2014-07-12 13:47:26 -0400

    Add statistics for logs

--- code/Log.py
+++ code/Log.py
@@ -4,6 +4,9 @@ Created on June 6, 2014
 @author Rainicy
 '''
 
+import re
+from datetime import *
+
 def writeLog(log, time, startCount, endCount):
 	'''
 	Description: Record a log in one-time crawling News Articles. 
@@ -59,3 +62,78 @@ def writeStartTime(log, startTime):
 	with open(log, 'a') as file: 
 		file.write('Start Crawling At: %s\n'% startTime)
 		file.close()
+
+def averageComments():
+	'''
+	Description: Calculate the average comments per news.
+
+	@input:
+		comments.log
+	@print
+		Total News:
+		Total Comments:
+		Average Comments Per News:
+	'''
+	with open('../log/comments.log', 'r') as file:
+		lines = file.readlines()
+		file.close()
+
+	count = 0
+	total_count = 0
+	r_total = re.compile(r'total: (.*)')
+	for line in lines:
+		line = line.split('\n')[0]
+		items = line.split('\t')
+		if len(items) == 5:
+			total = re.search(r_total, items[1])
+			total = int(total.group(1))
+			total_count += total
+			count += 1
+
+	print "Total News: {} \t Total Comments: {}".format(count, total_count)
+	print "Average Comments Per News: {}".format(total_count/count)
+
+
+def averageNews():
+	'''
+	Description: Calculate how many news crawling for one day.
+
+	@input:
+		'news.log'
+	@print:
+		Total Days:
+		Total News:
+		Average News per Day:
+	'''
+	with open('../log/news.log', 'r') as file:
+		lines = file.readlines()
+		file.close()
+
+	startTime = datetime(2014, 06, 07, 00, 00, 00)
+	r_total = re.compile(r'total: (.*)')
+	count_by_day = []
+	day = 0
+	count = 0
+	for line in lines:
+		line = line.split('\n')[0]
+		items = line.split('\t')
+		time = datetime.strptime(items[0], "%Y-%m-%d %H:%M:%S.%f")
+		delta_day = (time - startTime).days
+		if delta_day >= 0:
+			if delta_day == day:
+				total = re.search(r_total, items[1])
+				total = int(total.group(1))
+				count += total
+			else:
+				count_by_day.append(count)
+				day += 1
+				count = 0
+				total = re.search(r_total, items[1])
+				total = int(total.group(1))
+				count += total
+
+	total = 0
+	for c in count_by_day:
+		total += c
+	print "Total Days: {} \t Total News: {}".format(len(count_by_day), total)
+	print "Average News Per Day: {}".format(total/len(count_by_day))
